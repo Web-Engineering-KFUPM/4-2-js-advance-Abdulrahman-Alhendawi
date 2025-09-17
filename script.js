4a62eeeec3ba1e902e07607a4d67392fdd7ff4c5
Abdulrahman Alhendawi <a-rh2013@hotmail.com>
2025-09-17 09:06:16 +0300

    Task1

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ INSTRUCTIONS:
 */
 
 //You can uncomment the console.log code to check if your js file is connected with html file or not.
-//console.log("%cJS Lab Connected — Start completing the TODOs for Advance JavaScript!", "font-weight:bold; font-size:14px");
+console.log("%cJS Lab Connected — Start completing the TODOs for Advance JavaScript!", "font-weight:bold; font-size:14px");
 
 // ==========================
 // TODO-1: OBJECT with GETTERS & SETTERS
@@ -21,6 +21,37 @@ Task:
 3) Add a setter updateGpa(newGpa) or use a set accessor for gpa that validates 0.0–4.0.
 4) Create an instance/object and output its attributes using the getter(s).
 */
+
+class Student {
+   constructor(firstName, lastName, gpa) {
+   this.firstName = firstName;
+   this.lastName = lastName;
+   this._gpa = gpa;
+}
+get fullName() {
+    return this.firstName + " " + this.lastName;
+    }
+
+get gpa() {
+   return this._gpa;
+}
+set updateGpa(newGpa) {
+   if(newGpa >= 0.0 && newGpa <= 4.0) {
+      this._gpa = newGpa;
+   } else {
+      console.log("Invalid GPA");
+   }
+}
+}
+
+student1 = new Student("Abdulrahman", "Alhendawi", 3.83);
+
+console.log(student1.fullName);
+console.log(student1.gpa);
+student1.updateGpa = 4.4;
+console.log(student1.gpa)
+student1.updateGpa = 3.32
+console.log(student1.gpa)
 
 // ====================================
 // TODO-2: OBJECT AS MAP + for...in LOOP
